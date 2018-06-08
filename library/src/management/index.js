b1c5868f9b424303cd5d87b9a9812a006ded082f
刘昔诺 <liuxinuo@glzc.com>
2018-06-08 14:00:20 +0800

    构建vue

---
+++ library/src/management/index.js
@@ -0,0 +1,7 @@
+import Page from './Page'
+const routes = {
+  path: '/',
+  name: 'management',
+  component: Page
+}
+export default routes
--- library/src/router/index.js
+++ library/src/router/index.js
@@ -1,15 +1,14 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+// import managementRoutes from '../management/index'
+import ManagementPage from '@/management/index'
 
 Vue.use(Router)
 
-export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    }
-  ]
+const router = new Router({
+  mode: 'history',
+  routes: []
 })
+router.addRoutes([{...ManagementPage}])
+
+export default router
